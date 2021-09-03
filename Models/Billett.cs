5a832072e986ecfed3fabf3bc03e12e170d7a3f6
zaktaw <zaktaw@gmail.com>
2021-09-03 12:56:29 +0200

    Added attributes to class Billett

--- Models/Billett.cs
+++ Models/Billett.cs
@@ -5,7 +5,14 @@ using System.Threading.Tasks;
 
 namespace Mappe1_ITPE3200.Models
 {
-    public class Billett
-    {
-    }
+	public class Billett
+	{
+		[Key]
+		public int Id { get; set; }
+		public int AvgangId { get; set; }
+		public int KundeId { get; set; }
+		public double TotalPris { get; set; }
+		public bool Bilplass { get; set; }
+		public List<Lugar> lugarer { get; set; }
+	}
 }
