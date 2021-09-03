c72520fdcd520992f0d6c0dd7ee45970c47ad7bc
matsovevada <55790581+matsovevada@users.noreply.github.com>
2021-09-03 12:53:42 +0200

    Added attributes in Kunde.cs

--- Models/Kunde.cs
+++ Models/Kunde.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,13 @@ namespace Mappe1_ITPE3200.Models
 {
     public class Kunde
     {
-    }
+		[Key]
+		public int Id { get; set; }
+		public String Fornavn { get; set; }
+		public String Etternavn { get; set; }
+		public String Adresse { get; set; }
+		public String Postnr { get; set; }
+		public String Telefonnummer { get; set; }
+		public String Epost { get; set; }
+	}
 }
