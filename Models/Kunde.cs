e404c5eb3ccbb8608fa9a0b4ae7af06e449339a0
zaktaw <zaktaw@gmail.com>
2021-09-03 13:02:19 +0200

    Added attributed to Billett.cs

--- Models/Kunde.cs
+++ Models/Kunde.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,13 +7,5 @@ namespace Mappe1_ITPE3200.Models
 {
     public class Kunde
     {
-		[Key]
-		public int Id { get; set; }
-		public String Fornavn { get; set; }
-		public String Etternavn { get; set; }
-		public String Adresse { get; set; }
-		public String Postnr { get; set; }
-		public String Telefonnummer { get; set; }
-		public String Epost { get; set; }
-	}
+    }
 }
