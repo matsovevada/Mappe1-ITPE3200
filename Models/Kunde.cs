5cfed97e81764fe1e150e752b51abd2bbbdf25d8
zaktaw <zaktaw@gmail.com>
2021-09-03 13:07:20 +0200

    Fix merge error

--- Models/Kunde.cs
+++ Models/Kunde.cs
@@ -7,5 +7,13 @@ namespace Mappe1_ITPE3200.Models
 {
     public class Kunde
     {
+               [Key]
+               public int Id { get; set; }
+               public String Fornavn { get; set; }
+               public String Etternavn { get; set; }
+               public String Adresse { get; set; }
+               public String Postnr { get; set; }
+               public String Telefonnummer { get; set; }
+               public String Epost { get; set; }
     }
 }
