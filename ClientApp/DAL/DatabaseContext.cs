d9112fa1e14c697b8c1d0bbbf8335e389d3256aa
Peter <peter.stjernsund@gmail.com>
2021-09-10 10:22:55 +0200

    More seeding

--- ClientApp/DAL/DatabaseContext.cs
+++ ClientApp/DAL/DatabaseContext.cs
@@ -29,9 +29,11 @@ namespace Mappe1_ITPE3200.Models
 
         public class Lugar
         {
-            public string Type { get; set; }
+            public string Romkode { get; set; }
+            public string Beskrivelse { get; set; }
             public int AntallSengeplasser { get; set; }
-            public int AntallLedigeAvType { get; set; }
+            public int Antall { get; set; }
+            public int AntallLedige { get; set; }
             public int Pris { get; set; }
         }
 
