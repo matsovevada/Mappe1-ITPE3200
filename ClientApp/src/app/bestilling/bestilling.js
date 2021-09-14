9e079a8dda4ef1e0fc7724d7fb37977315f0e582
matsovevada <55790581+matsovevada@users.noreply.github.com>
2021-09-14 14:06:46 +0200

    Refactored avgang and dropdownStrekning into one component bestilling

---
+++ ClientApp/src/app/bestilling/bestilling.js
@@ -0,0 +1,1 @@
+//# sourceMappingURL=bestilling.js.map
--- ClientApp/src/app/bestilling/bestilling.ts
+++ ClientApp/src/app/bestilling/bestilling.ts
@@ -2,27 +2,41 @@ import { Component, OnInit } from "@angular/core";
 import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
 import { Avgang } from '../Avgang'
+import { Strekning } from '../Strekning'
 
 @Component({
-  selector: 'avganger',
-  templateUrl: 'avganger.html'
+  selector: 'bestilling',
+  templateUrl: 'bestilling.html'
 })
-export class avganger {
+export class Bestilling {
   alleAvganger: Array<Avgang>;
+  alleStrekninger: Array<Strekning>;
+  strekning: String;
   laster: boolean;
 
   constructor(private http: HttpClient, private router: Router) { }
 
   ngOnInit() {
     this.laster = true;
-    this.hentAlleAvganger();
+    this.hentAlleStrekninger();
   }
 
   hentAlleAvganger() {
-    this.http.get<Avgang[]>("api/Bestilling/")
+    this.http.get<Avgang[]>("api/Bestilling/hentAvgang/")
       .subscribe(avgangene => {
         console.log(avgangene)
         this.alleAvganger = avgangene;
+        this.laster = false;
+      },
+        error => console.log(error)
+      );
+  }
+
+  hentAlleStrekninger() {
+    this.http.get<Strekning[]>("api/Bestilling/hentStrekning")
+      .subscribe(strekningene => {
+        console.log(strekningene)
+        this.alleStrekninger = strekningene;
         this.laster = false;
       },
         error => console.log(error)
