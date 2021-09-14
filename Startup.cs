d8a58923b2d7cc840063cd1b10560a995a8affec
matsovevada <55790581+matsovevada@users.noreply.github.com>
2021-09-14 10:54:09 +0200

    Update Startup.cs

--- Startup.cs
+++ Startup.cs
@@ -23,19 +23,17 @@ namespace Mappe1_ITPE3200
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllersWithViews();
+            // må ha med dette for å sette opp databasen
+            services.AddControllers();
+            services.AddDbContext<DbContext>(options =>
+                options.UseSqlite("Data source=Bestillinger.db"));
+            services.AddScoped<IBestillingRepository, BestillingRepository>();
+
             // In production, the Angular files will be served from this directory
             services.AddSpaStaticFiles(configuration =>
             {
                 configuration.RootPath = "ClientApp/dist";
             });
-
-            // m? ha med dette for ? sette opp databasen
-            services.AddControllers();
-            services.AddConnections();
-            services.AddDbContext<DbContext>(options => options.UseSqlite("Data source=Bestillinger.db"));
-
-            services.AddScoped<IBestillingRepository, BestillingRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
