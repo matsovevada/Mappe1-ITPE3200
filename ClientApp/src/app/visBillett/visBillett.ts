f69dc15ae100dd3444571f882d200a88e0e12229
Peter <peter.stjernsund@gmail.com>
2021-09-21 13:13:41 +0200

    visBillett

---
+++ ClientApp/src/app/visBillett/visBillett.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from "@angular/core";
+import { HttpClient } from '@angular/common/http';
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Kunde } from "../Kunde";
+
+@Component({
+  selector: 'visBillett',
+  templateUrl: "visBillett.html"
+})
+
+
+export class visBillett {
+
+} 
+
+visBillett() {
+
+  };
+}
