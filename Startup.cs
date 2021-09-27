96702a2fa09951c3ca86d85486856377e9922ab2
matsovevada <55790581+matsovevada@users.noreply.github.com>
2021-09-27 12:13:36 +0200

    Added logging for posts and gets

--- Startup.cs
+++ Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 namespace Mappe1_ITPE3200
 {
@@ -41,11 +42,12 @@ namespace Mappe1_ITPE3200
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILoggerFactory loggerFactory)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                loggerFactory.AddFile("Logs/log.txt");
                 DBinit.Seed(app);
             }
             else
