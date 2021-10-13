83556296914c5db5e43aaa345a23dcac43eccb45
matsovevada <55790581+matsovevada@users.noreply.github.com>
2021-10-13 10:27:36 +0200

    changes that didnt get saved before the previous commit

--- ClientApp/src/app/admin/meny/meny.ts
+++ ClientApp/src/app/admin/meny/meny.ts
@@ -1,1 +1,12 @@
-﻿
+import { Component, ViewEncapsulation } from "@angular/core";
+
+@Component({
+  selector: 'meny',
+  templateUrl: 'meny.html',
+  styleUrls: ['meny.css'],
+  encapsulation: ViewEncapsulation.None
+})
+
+export class Meny {
+
+}
--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -13,6 +13,7 @@ import { LugarValgRetur } from './lugarValgRetur/lugarValgRetur';
 import { visBillett } from './visBillett/visBillett';
 import { LugarComponent } from './lugarComponent/lugarComponent';
 import { Navbar } from "./navbar/navbar";
+import { Meny } from "./admin/meny/meny";
 
 @NgModule({
   declarations: [
@@ -23,7 +24,8 @@ import { Navbar } from "./navbar/navbar";
     LugarValgRetur,
     visBillett,
     LugarComponent,
-    Navbar
+    Navbar,
+    Meny
   ],
 
   imports: [
