7a35dc30b9ac73bd85eecebdbdc881ed6eea9963
Peter <peter.stjernsund@gmail.com>
2021-11-04 11:15:55 +0100

    Regex bruker

--- Models/Bruker.cs
+++ Models/Bruker.cs
@@ -1,8 +1,12 @@
-﻿namespace Mappe1_ITPE3200.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Mappe1_ITPE3200.Models
 {
     public class Bruker
     {
+        [RegularExpression(@"^[a-zA-ZæøåÆØÅ. \-]{2,20}$")]
         public string Brukernavn { get; set; }
+        [RegularExpression(@"^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{6,}$")]
         public string Passord { get; set; }
     }
 }
